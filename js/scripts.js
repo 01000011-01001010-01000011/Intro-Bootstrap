fdc4552ac84b9bf872fde44bb5888871c742da15
01000011-01001010-01000011 <pctweakeraddiction@hotmail.com>
2021-06-18 16:34:15 -0400

    less and scss

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,51 @@
-$(document).ready(function () {
-   
-    $('[data-toggle="tooltip"]').tooltip();
-   
+/* TWO BUTTONS
+
+$(document).ready(function(){
+
+    $("#mycarousel").carousel({ interval: 2000 });
+  
+    $("#carousel-pause").click(function() {
+
+        $("#mycarousel").carousel("pause");
+
+    });
+
+    $("#carousel-play").click(function() {
+
+        $("#mycarousel").carousel("cycle");
+
+    });
+
+});
+
+*/
+
+
+$(document).ready(function(){
+
+    $("#mycarousel").carousel({ interval: 2000 });
+  
+    $("#carouselButton").click(function() {
+        
+        if($("#carouselButton").children("span").hasClass("fa-pause")){
+
+            $("#mycarousel").carousel("pause");
+
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            
+            $("#carouselButton").children("span").addClass("fa-play");
+
+        }
+        else{           
+
+            $("#mycarousel").carousel("cycle");
+
+            $("#carouselButton").children("span").removeClass("fa-play");
+
+            $("#carouselButton").children("span").addClass("fa-pause");
+
+        }    
+
+    });
+
 });
